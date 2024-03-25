b537c4e96b6d1bef27f53b47a200c1bae7b4e00e
Kalucza Gábor <kaluczadzsi@gmail.com>
2024-03-25 14:17:55 +0100

    event props

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,13 @@
-import Greet from './components/Greet';
-import Heading from './components/Heading';
-import Oscar from './components/Oscar';
-import Person from './components/Person';
-import PersonList from './components/PersonList';
-import Status from './components/Status';
+import Button from './components/Button';
+import Input from './components/Input';
 
 const App = () => {
-  const personName = {
-    first: 'Bruce',
-    last: 'Wayne',
-  };
-
-  const nameList = [
-    {
-      first: 'Bruce',
-      last: 'Wayne',
-    },
-    {
-      first: 'Clark',
-      last: 'Kent',
-    },
-    {
-      first: 'Princess',
-      last: 'Diana',
-    },
-  ];
-
   return (
-    <div className="app">
-      <Greet name={'Tony'} messageCount={10} isLoggedIn={false} />
-      <Person name={personName} />
-      <PersonList names={nameList} />
-      <Status status="success" />
-
-      <Oscar>
-        <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
-      </Oscar>
-      <Greet name={'Mike'} isLoggedIn={true} />
+    <div>
+      <Button
+        handleClick={(event, id) => console.log('Button clicked', event, id)}
+      />
+      <Input value="" handleChange={(event) => console.log(event)} />
     </div>
   );
 };
---
+++ src/components/Input.tsx
@@ -0,0 +1,10 @@
+type InputProps = {
+  value: string;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const Input = ({ value, handleChange }: InputProps) => {
+  return <input type="text" value={value} onChange={handleChange}></input>;
+};
+
+export default Input;
